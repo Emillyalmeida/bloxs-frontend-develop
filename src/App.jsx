c1fc1fb6b51353af3f18c387ff8c730842fd6ddf
emillyalmeida <emillyalmeidan@gmail.com>
2022-07-11 16:36:30 -0300

    feat: add component divCrd and useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { useEffect } from "react";
+
 import Aside from "./components/aside";
 import Container from "./components/container";
 import Content from "./components/content";
+import DivCard from "./components/divCards";
 import Header from "./components/header";
+import useNotice from "./providers/notices";
+
 import GlobalStyle from "./style/global";
 
 function App() {
+  const { GetAgro, GetEnergy } = useNotice();
+
+  useEffect(() => {
+    GetEnergy();
+    GetAgro();
+  }, []);
+
   return (
     <>
       <GlobalStyle />
@@ -12,6 +24,7 @@ function App() {
         <Aside />
         <Content>
           <Header title="Noticias" />
+          <DivCard />
         </Content>
       </Container>
     </>
