46c4832819b1f352b1ad2fe10f235ec21ea02264
emillyalmeida <emillyalmeidan@gmail.com>
2022-07-11 14:57:50 -0300

    feat: add components content and header in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Aside from "./components/aside";
 import Container from "./components/container";
+import Content from "./components/content";
+import Header from "./components/header";
 import GlobalStyle from "./style/global";
 
 function App() {
@@ -8,6 +10,9 @@ function App() {
       <GlobalStyle />
       <Container>
         <Aside />
+        <Content>
+          <Header title="Noticias" />
+        </Content>
       </Container>
     </>
   );
