f4a94edc3a9d289008d7450c96bd21dfdcc65537
emillyalmeida <emillyalmeidan@gmail.com>
2022-07-14 10:04:48 -0300

    test: add cypress

--- src/__test__/components/header.test.jsx
+++ src/__test__/components/header.test.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import Header from "../../components/header";
+import useTheme, { ColorProvider, ColorContext } from "../../providers/theme";
+import { ThemeProvider } from "styled-components";
+import { themes } from "../../style/theme";
 
 describe("Component Header", () => {
   test("should be able to render header", () => {
@@ -8,4 +11,30 @@ describe("Component Header", () => {
 
     expect(screen.getByText(/test/)).toBeInTheDocument();
   });
+
+  // test("should change colors", async () => {
+  //   // const { currentTheme, setCurrentTheme, getOpositeTheme } =
+  //   //   React.useContext(ColorContext);
+  //   const { getOpositeTheme } = useTheme();
+  //   const test = {
+  //     currentTheme: "light",
+  //   };
+  //   const mockOposite = {
+  //     getOpositeTheme: () => "dark",
+  //   };
+
+  //   render(
+  //     <ColorContext.Provider value={{ test, mockOposite,setCurrentTheme }}>
+  //       <ThemeProvider theme={themes[test.currentTheme]}>
+  //         <Header title={"test"} />
+  //       </ThemeProvider>
+  //     </ColorContext.Provider>
+  //   );
+
+  //   fireEvent.click(screen.getByRole("button"));
+
+  //   await waitFor(() => {
+  //     expect(screen.getByText("test")).toHaveStyle("color: #171923");
+  //   });
+  // });
 });
